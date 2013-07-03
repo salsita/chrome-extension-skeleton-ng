bbd124c5f6b53a140d84ab927a41391f530d6bbe
Igor Lobiv <igor@salsitasoft.com>
2013-07-03 12:32:24 +0200

    try to add content script

---
+++ code/js/content.js
@@ -0,0 +1,60 @@
+requirejs.config(requireConfig);
+
+requirejs([ 'jquery',
+            'config',
+            'util/messaging',
+            'util/messagingClient',
+            'lib/purl',
+            'logging',
+            'angular'],
+function(   $,
+            config,
+            messaging,
+            client,
+            purl,
+            logging,
+            angular) {
+
+  // uncomment the following line if content should be handling some requests
+  // sent from background (when appropriate handler is implemented in
+  // contentHandlers.js
+
+  messaging.contentInitialize();
+  logging.init(true, "content", client);
+  logging.debug("content started");
+
+  'use strict';
+  client.sendBroadcast({
+    cmd: 'LoadHtml',
+    args: {
+      template: 'content',
+      data: {}
+    }
+  }, function(response) {
+    $(function() {
+      // inject html to content page
+      $(response)
+      .appendTo('body');
+
+      var contentDiv = $('#contentDiv');
+
+      var contentApp = angular.module("contentApp", []);
+      contentApp.controller("contentController", function($scope, $http){
+        $scope.host = "www.test.com";
+      });
+
+      angular.bootstrap(contentDiv, [contentApp['name']]);
+      // Because of RequireJS we need to bootstrap the app app manually
+      // and Angular Scenario runner won't be able to communicate with our app
+      // unless we explicitely mark the container as app holder
+      // More info: https://groups.google.com/forum/#!msg/angular/yslVnZh9Yjk/MLi3VGXZLeMJ
+      contentDiv.addClass('ng-app');
+
+      // due to chrome extension can work only in CSP (Content Security Policy)
+       // more details http://docs.angularjs.org/api/ng.directive:ngCsp
+      contentDiv.addClass('ng-csp');
+    });
+  });
+
+
+});
